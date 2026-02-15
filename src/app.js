1c411639d12fe4913c5daa5db68717e43cc63022
lucasprietoalmeida <lucasprietoalmeidaan@gmail.com>
2026-02-15 16:56:36 +0100

    feat: add jwt auth middleware and protected route

--- src/app.js
+++ src/app.js
@@ -11,4 +11,11 @@ app.get('/health', (req, res) => {
 
 app.use('/auth', authRoutes);
 
+const authMiddleware = require('./middlewares/auth.middleware');
+
+app.get('/users/me', authMiddleware, (req, res) => {
+    res.json(req.user);
+});
+
+
 module.exports = app;
